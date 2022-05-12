1937c66c7a85e08b2c66d4ce683cc73d029e0978
pifragile <pigu@bluewin.ch>
2022-05-12 16:00:48 +0200

    init react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "bulma/css/bulma.min.css";
+import "./App.css";
+import Test from "./components/Test";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div classname="main">
+            {["hello", "world", "huhu"].map((x) => (
+                <Test val={x} />
+            ))}
+          <div>Hello, World</div>
+        </div>
+    );
 }
 
 export default App;
