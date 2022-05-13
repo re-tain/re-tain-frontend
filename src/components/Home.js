03a746e907cf63d11c945ee7f8cc73954e4c796e
pifragile <pigu@bluewin.ch>
2022-05-13 14:34:53 +0200

    implement flex and homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,21 @@ function Home() {
     return (
         <Layout>
             <div className="main">
+            <h1>About EditArt</h1>
+                <div>
+                    EditArt is a platform for generative art, where collectors can co-create a piece of art with the artist.
+                </div>
+                <h1>Token Overview</h1>
+                <div style={
+                    {
+                        display: 'flex',
+                        flexWrap: 'wrap'
+                    }
+                }>
                 {["hello", "world", "huhu", "eis", "a", "s", "h", "e", "ui", "oii"].map((x) => (
                     <TokenBox title={x} key={x}/>
                 ))}
+                </div>
             </div>
         </Layout>
     );
