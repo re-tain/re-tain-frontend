2d515de3de54c1b85e9970c290b412f4f2e1c73a
pifragile <pigu@bluewin.ch>
2022-05-13 17:44:32 +0200

    add info

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,45 @@ function Home() {
     return (
         <Layout>
             <div className="main">
-            <h1>About EditArt</h1>
+                <h1>EditArt...</h1>
+
+                <ul>
+                    <li>is a generative art platform, where collectors
+                    can co-create a piece of art with the artist.</li>
+                    <li>is currently running in Beta mode and artists
+                    wishing to release a token can do so, but by
+                    invite only.</li>
+                    <li>is a project by generative artist <a href="https://twitter.com/pifragile/"> pifragile</a></li>
+                </ul>
                 <div>
-                    EditArt is a platform for generative art, where collectors can co-create a piece of art with the artist.
+                    For infos <a href="https://twitter.com/pifragile/"> DM me on twitter</a> 
                 </div>
+
+                <div>
+                ❤️ , pifragile
+                </div>
+                
                 <h1>Token Overview</h1>
-                <div style={
-                    {
-                        display: 'flex',
-                        flexWrap: 'wrap'
-                    }
-                }>
-                {["hello", "world", "huhu", "eis", "a", "s", "h", "e", "ui", "oii"].map((x) => (
-                    <TokenBox title={x} key={x}/>
-                ))}
+                <div
+                    style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                    }}
+                >
+                    {[
+                        "hello",
+                        "world",
+                        "huhu",
+                        "eis",
+                        "a",
+                        "s",
+                        "h",
+                        "e",
+                        "ui",
+                        "oii",
+                    ].map((x) => (
+                        <TokenBox title={x} key={x} />
+                    ))}
                 </div>
             </div>
         </Layout>
