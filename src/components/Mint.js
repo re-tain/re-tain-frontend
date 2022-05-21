824b3416299c2ce68c97d21d91fac03534f07382
pifragile <pigu@bluewin.ch>
2022-05-21 21:59:18 +0200

    implement mint preview

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -1,7 +1,12 @@
 import Layout from "./Layout";
 import MintForm from "./MintForm";
+import { useState } from "react";
 
 function Mint() {
+    const [queryString, setQueryString] = useState("m0=0.5&m1=0.5&m2=0.5&m3=0.5&m4=0.5");
+    let setSrc = (m0, m1, m2, m3, m4) => {
+        setQueryString(`m0=${m0}&m1=${m1}&m2=${m2}&m3=${m3}&m4=${m4}`);
+    };
     return (
         <Layout>
             <h1>Mint</h1>
@@ -17,7 +22,7 @@ function Mint() {
                         height: "400px",
                         width: "400px",
                     }}
-                    src="https://pifragile.com/ab0/"
+                    src={"https://pifragile.com/eatest/?" + queryString}
                 ></iframe>
             </div>
 
@@ -29,7 +34,7 @@ function Mint() {
                 }}
             >
                 <div style={{ width: "400px" }}>
-                    <MintForm />
+                    <MintForm onSubmitForm={setSrc} />
                 </div>
             </div>
         </Layout>
--- src/components/MintForm.js
+++ src/components/MintForm.js
@@ -1,15 +1,73 @@
-function MintForm() {
+import React from "react";
+
+function MintForm({ onSubmitForm }) {
+    let handleChange = (e) => {
+        e.preventDefault();
+        onSubmitForm(
+            e.target.form.value0.value,
+            e.target.form.value1.value,
+            e.target.form.value2.value,
+            e.target.form.value3.value,
+            e.target.form.value4.value,
+
+        );
+    };
     return (
         <div>
-            <form action="#">
+            <form onChange={handleChange}>
                 <fieldset>
                     <legend>Sliders</legend>
                     <div className="form-group">
-                        <input className="mint-slider" type="range" min="1" max="100" id="value0"/>
-                        <input className="mint-slider" type="range" min="1" max="100" id="value1"/>
-                        <input className="mint-slider" type="range" min="1" max="100" id="value2"/>
-                        <input className="mint-slider" type="range" min="1" max="100" id="value3"/>
-                        <input className="mint-slider" type="range" min="1" max="100" id="value4"/>
+                        <input
+                            className="mint-slider"
+                            type="range"
+                            min="0"
+                            max="1"
+                            defaultValue="0.5"
+                            step="0.001"
+                            id="value0"
+                            name="value0"
+                        />
+                        <input
+                            className="mint-slider"
+                            type="range"
+                            min="0"
+                            max="1"
+                            defaultValue="0.5"
+                            step="0.001"
+                            id="value1"
+                            name="value1"
+                        />
+                        <input
+                            className="mint-slider"
+                            type="range"
+                            min="0"
+                            max="1"
+                            defaultValue="0.5"
+                            step="0.001"
+                            id="value2"
+                            name="value2"
+                        />
+                        <input
+                            className="mint-slider"
+                            type="range"
+                            min="0"
+                            max="1"
+                            defaultValue="0.5"
+                            step="0.001"
+                            id="value3"
+                            name="value3"
+                        />
+                        <input
+                            className="mint-slider"
+                            type="range"
+                            min="0"
+                            max="1"
+                            defaultValue="0.5"
+                            step="0.001"
+                            id="value4"
+                            name="value4"
+                        />
                     </div>
                     <div className="form-group">
                         <button
