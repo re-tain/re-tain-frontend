afb792cc46e1b2e454b20fc002c448a147866cce
pifragile <pigu@bluewin.ch>
2022-06-05 14:31:09 +0200

    configure env loading

--- src/contracts.js
+++ src/contracts.js
@@ -1,14 +1,32 @@
-let contractList = [
-    {
-        name: "EATEST EIS",
-        address: "KT1TzkQSbrS8qeJ1iTbXZufizpXq57QELhgp",
-        author: "pifragile"
-    },
-    {
-        name: "EATEST ZWEI",
-        address: "KT1UHpfAcxmCNE5SusasBpBJKwmdU37EVjyP",
-        author: "Some Person"
-    },
-];
+import { ENV } from "./consts";
 
-export default contractList
+let contractList;
+if (ENV === "dev") {
+    contractList = [
+        {
+            name: "EATEST EIS",
+            address: "KT1TzkQSbrS8qeJ1iTbXZufizpXq57QELhgp",
+            author: "pifragile",
+        },
+        {
+            name: "EATEST ZWEI",
+            address: "KT1UHpfAcxmCNE5SusasBpBJKwmdU37EVjyP",
+            author: "Some Person",
+        },
+    ];
+} else if (ENV === "prod") {
+    contractList = [
+        {
+            name: "EATEST EIS",
+            address: "KT1TzkQSbrS8qeJ1iTbXZufizpXq57QELhgp",
+            author: "pifragile",
+        },
+        {
+            name: "EATEST ZWEI",
+            address: "KT1UHpfAcxmCNE5SusasBpBJKwmdU37EVjyP",
+            author: "Some Person",
+        },
+    ];
+}
+
+export default contractList;
