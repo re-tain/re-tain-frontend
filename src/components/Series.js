4d2af632d5b229ab8c2f8b63f993777363c6df3c
pifragile <pigu@bluewin.ch>
2022-08-04 09:04:20 +0200

    cleanup

--- src/components/Series.js
+++ src/components/Series.js
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 import { getContractStorage, getContractMetadata, getToken } from "../lib/api";
 import UserDetail from "./UserDetail";
 import MarketPlace from "./Marketplace";
-import MintButton from "./MintButton";
 import TokenImage from "./TokenImage";
 
 function Series() {
@@ -19,7 +18,7 @@ function Series() {
     const [artist, setArtist] = useState(null);
     const [previewArtifactUrl, setPreviewArtifactUrl] = useState(null);
     const [previewDisplayUrl, setPreviewDisplayUrl] = useState(null);
-    const [paused, setPaused] = useState(null);
+    const [_paused, setPaused] = useState(null);
 
     useEffect(() => {
         const fetchStorage = async () => {
