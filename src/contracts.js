a240eb36285fb47f8b0abff19a07aca4bf901523
pifragile <pigu@bluewin.ch>
2022-08-04 09:27:26 +0200

    set new contract

--- src/contracts.js
+++ src/contracts.js
@@ -5,7 +5,7 @@ if (["dev", "staging"].includes(ENV)) {
     contractList = [
         {
             name: "EATEST EIS",
-            address: "KT18rHLA8S4meAWQc5T6PoJxz7jAZhfCiNRo",
+            address: "KT1P7BCwZ6EWiNyF9bTGHdCgw7uSwbJGPxb6",
             author: "pifragile",
         },
     ];
