ed090eeb9922ee88fc20fd96b11c8e0f45a565dd
pifragile <pigu@bluewin.ch>
2022-08-25 16:59:39 +0200

    add genesis

--- src/contracts.js
+++ src/contracts.js
@@ -8,6 +8,11 @@ if (["dev", "staging"].includes(ENV)) {
             address: "KT1MN45jArZpp7nt63yE9drs6sXoQ6A2cMt7",
             author: "pifragile",
         },
+        {
+            name: "Genesis",
+            address: "KT1V3BPpc98BbL6wXn7m9AZJx5i5Xp8b4LTU",
+            author: "pifragile",
+        },
     ];
 } else if (ENV === "prod") {
     contractList = [];
