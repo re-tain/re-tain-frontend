987a3158d4ba6356bbb946a2fb1d790f6ee9e43e
pifragile <pigu@bluewin.ch>
2022-10-17 19:07:48 +0800

    change testnet to ghostnet

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
-export const TEZOS_NETWORK = process.env.REACT_APP_TEZOS_NETWORK || 'jakartanet';
-export const TZKT_API = process.env.REACT_APP_TZKT_API || 'https://api.jakartanet.tzkt.io/';
-export const RPC_NODE = process.env.REACT_APP_RPC_NODE || 'https://jakartanet.ecadinfra.com';
+export const TEZOS_NETWORK = process.env.REACT_APP_TEZOS_NETWORK || 'ghostnet';
+export const TZKT_API = process.env.REACT_APP_TZKT_API || 'https://api.ghostnet.tzkt.io/';
+export const RPC_NODE = process.env.REACT_APP_RPC_NODE || 'https://ghostnet.tezos.marigold.dev/';
 export const OBJKT_API = 'https://data.objkt.com/v2/graphql'
 export const IPFS_GATEWAY = 'https://pifragile.mypinata.cloud/ipfs/'
 export const ENV = process.env.REACT_APP_ENV || 'dev';
