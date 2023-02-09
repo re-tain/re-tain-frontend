c35ada7e235a08826e9117e41b0109d856c611c5
pifragile <pigu@bluewin.ch>
2023-02-09 11:34:54 +0800

    exclude old series

--- src/components/SeriesOverview.js
+++ src/components/SeriesOverview.js
@@ -31,6 +31,7 @@ function SeriesOverview() {
                     `${separator}limit=${pageLength}&offset=${page}`
             );
             let result = await res.json();
+            result = result.filter(e => e.firstActivity >= 1922363) 
             if (result.length > 0) {
                 const newContracts = await Promise.all(
                     result.map(async (c) => {
