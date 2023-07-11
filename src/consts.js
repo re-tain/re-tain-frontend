80363fd32ddd2ff9319e974fc909ae5b7fc89303
pifragile <pigu@bluewin.ch>
2023-07-11 10:33:45 +0200

    gateway

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
 export const TEZOS_NETWORK = process.env.REACT_APP_TEZOS_NETWORK || 'ghostnet';
 export const TZKT_API = process.env.REACT_APP_TZKT_API || 'https://api.ghostnet.tzkt.io/';
-export const RPC_NODE = process.env.REACT_APP_RPC_NODE || 'https://ghostnet.tezos.marigold.dev/';
+export const RPC_NODE = process.env.REACT_APP_RPC_NODE || 'https://ghostnet.ecadinfra.com'//'https://ghostnet.tezos.marigold.dev/';
 export const OBJKT_API = 'https://data.objkt.com/v3/graphql'
-export const IPFS_GATEWAY = 'https://re-tain.mypinata.cloud/ipfs/'
+export const IPFS_GATEWAY = 'https://amaranth-moral-porcupine-982.mypinata.cloud/ipfs/'
 export const UPLOAD_URL = process.env.REACT_APP_UPLOAD_URL || 'https://seashell-app-ly3q3.ondigitalocean.app/file-upload'
 export const ENV = process.env.REACT_APP_ENV || 'dev';
