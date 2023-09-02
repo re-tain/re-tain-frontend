bd13e27ff3478df15f05117289dd065fa1f2ac70
pifragile <pigu@bluewin.ch>
2023-09-02 15:00:27 +0800

    add dummy function to contract

--- src/contracts.js
+++ src/contracts.js
@@ -3,7 +3,7 @@ import { ENV } from "./consts";
 let referenceContract;
 
 if (["dev", "staging"].includes(ENV)) {
-    referenceContract = "KT1B1fDbP1FkZuHVpSpjhyFKWxBV4yziyQYQ";
+    referenceContract = "KT1MgbU11F1Q3GsAVPRK1JFmPLGh4AWFpzqg";
 } else if (ENV === "prod") {
     referenceContract = "KT199xqPrHQxibWtqq3GmYiob2isFJjRmTGV";
 }
