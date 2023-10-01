835ef5b8bcfa112350230d4c26c98f9acbd0560f
pifragile <pigu@bluewin.ch>
2023-10-01 09:40:46 +0800

    add tailwind

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,18 @@
 import Layout from "./Layout";
 function Home() {
-
     return (
         <Layout>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
             Own your art.
-            <br/>
-            <br/>
+            <br />
+            <br />
             Onchain metadata.
-            <br/>
-            <br/>
+            <br />
+            <br />
             Integrated marketplace.
-            <br/>
-            <br/>
+            <br />
+            <br />
             Deploy your own smart contract for your art project on Tezos now.
         </Layout>
     );
