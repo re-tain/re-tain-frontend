e7108ab156488e2bb93c3aaa3989e80eba2ccda9
pifragile <pigu@bluewin.ch>
2023-10-01 12:06:13 +0800

    fix build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import inject from "@rollup/plugin-inject";
+import rollupNodePolyFill from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,15 +10,24 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-          stream: "stream-browserify",
-          os: "os-browserify/browser",
-          util: "util",
-          process: "process/browser",
-          buffer: "buffer",
+            stream: "stream-browserify",
+            os: "os-browserify/browser",
+            util: "util",
+            process: "process/browser",
+            buffer: "buffer",
         },
-      },
+    },
     esbuild: {
         loader: "jsx",
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+        //https://stackoverflow.com/questions/70714690/buffer-is-not-defined-in-react-vite
+        rollupOptions: {
+            plugins: [rollupNodePolyFill()],
+        },
     },
     optimizeDeps: {
         esbuildOptions: {
@@ -33,7 +43,6 @@ export default defineConfig({
                     buffer: true,
                 }),
             ],
-
         },
     },
 });
