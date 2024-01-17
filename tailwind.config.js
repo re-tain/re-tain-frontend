676d306666ebe7b2920a9ee7d74a1f0f4320c106
pifragile <pigu@bluewin.ch>
2024-01-17 22:13:50 +0800

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const colors = require('tailwindcss/colors')
+
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+      colors: {
+        'brand': '#F0FF42',
+        'brandx': '#EDEDF0',
+             black: colors.black,
+        white: colors.white,
+        grey: colors.trueGray,
+        indigo: colors.indigo,
+        green: colors.green,
+        red: colors.rose,
+        yellow: colors.amber,
+      },
     extend: {},
+    // ... darkMode: 'class',
   },
-  plugins: [],
+  plugins: [
+    // ... 
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
